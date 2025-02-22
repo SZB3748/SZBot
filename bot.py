6722353ffc11e1726eb6ce4b68155fa2541429d4
SZB3748 <jakengoodyear@gmail.com>
2025-02-22 08:01:25 -0500

    Add bot commands to modify playerstate

--- bot.py
+++ bot.py
@@ -1,9 +1,11 @@
 import asyncio
 import config
+from datetime import datetime
 import requests
-import twitchio
 import traceback
+import twitchio
 from twitchio.ext import commands
+from typing import Any, Awaitable, Callable
 from urllib.parse import quote
 
 TOKEN_REFRESH_ENDPOINT = "https://id.twitch.tv/oauth2/token"
@@ -61,6 +63,13 @@ async def main():
         pass
 
 
+def log_command(f:Callable[..., Awaitable[Any]]):
+    async def wrapper(ctx:commands.Context, *args, **kwargs):
+        print(datetime.now().strftime("[%Y-%M-%d %H:%M:%S]"), f"{ctx.author}: {ctx.message.content}")
+        return await f(ctx, *args, **kwargs)
+    wrapper.__name__ = f.__name__
+    return wrapper
+
 if __name__ == "__main__":
     bot = init_bot()
 
@@ -69,12 +78,50 @@ if __name__ == "__main__":
         print("bot running")
 
     @bot.command(name="addsong")
+    @log_command
     async def add_song(ctx:commands.Context, url:str):
-        r = requests.post("http://localhost:8080/api/music/queue/push", data={"url": url})
+        r = requests.post("http://localhost:6742/api/music/queue/push", data={"url": url})
         if r.ok:
             print(f"Added song ({r.text})")
         else:
             print("Failed to add song")
+    
+    @bot.command(name="skipsong")
+    @log_command
+    async def skip_song(ctx:commands.Context, count:int=1):
+        if not ctx.author.is_mod: #also works for broadcaster
+            return
+        
+        count = int(count)
+        if count <= 0:
+            return
+        r = requests.post("http://localhost:6742/api/music/queue/skip", data={"count": count})
+        if r.ok:
+            print(f"Skipped", r.text, "songs")
+        else:
+            print("Failed to skip song")
+
+    @bot.command(name="pausesong")
+    @log_command
+    async def pause_song(ctx:commands.Context):
+        if not ctx.author.is_mod:
+            return
+        r = requests.post("http://localhost:6742/api/music/playerstate", data={"state": "pause"})
+        if r.ok:
+            print("Paused")
+        else:
+            print("Failed to pause")
+
+    @bot.command(name="playsong")
+    @log_command
+    async def pause_song(ctx:commands.Context):
+        if not ctx.author.is_mod:
+            return
+        r = requests.post("http://localhost:6742/api/music/playerstate", data={"state": "play"})
+        if r.ok:
+            print("Paused")
+        else:
+            print("Failed to pause")
 
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
